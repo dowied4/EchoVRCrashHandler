1029f7846b32e5cca2f0cd28e30d89d64841fd32
sealablebag dowie <dowied4@mymacewan.ca>
2020-07-13 15:28:01 -0600

    Removes echovr api dependancy and updated exe
    Now checks for current team for player to rejoin the correct team

--- Rejoin.py
+++ Rejoin.py
@@ -3,7 +3,11 @@ import psutil
 import time
 import sys
 import getopt
-import echovr_api
+import requests
+
+def fetch_state():
+    req = requests.get("http://127.0.0.1:6721/session")
+    return req.json()
 
 def findProcess():
     for process in psutil.process_iter():
@@ -12,26 +16,26 @@ def findProcess():
     return False
 
 def getTeam(state):
-    for player in state.teams[0]:
-        if player.name = state.client_name:
+    for player in state["teams"][0]["players"]:
+        if player["name"] == state["client_name"]:
             return 0
-    for player in state.teams[1]:
-        if player.name = state.client_name:
+    for player in state["teams"][1]["players"]:
+        if player["name"] == state["client_name"]:
             return 1
     return 2
 
-def getId(old):
+def getId(old,oldTeam):
     try:
-        gameState = echovr_api.fetch_state()
+        gameState = fetch_state()
         team = getTeam(gameState)
-        tempMatch = gameState.sessionid
+        tempMatch = gameState["sessionid"]
         if (tempMatch != old):
             print("found a new match with id: " + tempMatch)
             return (tempMatch,team)
         else:
             return (old,team)
     except:
-        return old
+        return (old,oldTeam)
 
 if __name__ == "__main__":
     try:
@@ -48,16 +52,23 @@ if __name__ == "__main__":
     time.sleep(5)
     matchId = ""
     team = 2
+    
     while True:
         if(findProcess()):
-            matchTuple = getId(matchId)
+            matchTuple = getId(matchId,team)
             matchId = matchTuple[0]
             team = matchTuple[1]
         else:
             print("Game Closed!")
             if matchId:
                 print("recovering to match: " + matchId)
-                command = "\"" + path + "\""  + " -lobbyid " + matchId + " -lobbyteam " + team
+                if team == 0:
+                    print("Joining Blue")
+                elif team == 1:
+                    print("Joining Orange")
+                elif team == 2:
+                    print("Joining Spectator")
+                command = "\"" + path + "\""  + " -lobbyid " + matchId + " -lobbyteam " + str(team)
                 subprocess.Popen(command)
             else:
                 print("No Match ID Stored")
